95755b76a4227dfe19d3e4db8d3b9a8a9dbdffb6
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2015-03-13 21:45:40 +0200

    Add simple-todos initial app

---
+++ simple-todos.js
@@ -0,0 +1,30 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+    Template.body.helpers({
+        tasks: function () {
+            return Tasks.find({}, {sort: {createdAt: -1}});
+        }
+    });
+    Template.body.events({
+        "submit .new-task": function (event) {
+            var text = event.target.text.value;
+
+            Tasks.insert({
+                text: text,
+                createdAt: new Date()
+            });
+
+            event.target.text.value = "";
+            return false;
+        }
+    });
+    Template.task.events({
+        "click .toggle-checked": function () {
+            Tasks.update(this._id, {$set: {checked: !this.checked}});
+        },
+        "click .delete": function() {
+            Tasks.remove(this._id);
+        }
+    });
+}
